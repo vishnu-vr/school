c88b37537588dddc1cd6b61b22ad103a4ea97508
vishnuramesh <vishnuramesh19@gmail.com>
2020-11-11 13:12:16 +0530

    added linq for GetOne and Delete operations

--- school/ManageStaff.cs
+++ school/ManageStaff.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace school
 {
@@ -9,20 +10,13 @@ namespace school
         public void AddStaff(Type type, string name, string email, int empCode, string extra)
         {
             //teacher
-            if (type == Type.teacher)
-            {
-                Staffs.Add(new Teacher(name,email,empCode, extra));
-            }
+            if (type == Type.teacher) Staffs.Add(new Teacher(name, email, empCode, extra));
+
             //admin
-            else if (type == Type.administrator)
-            {
-                Staffs.Add(new Administartor(name, email, empCode, extra));
-            }
+            else if (type == Type.administrator) Staffs.Add(new Administartor(name, email, empCode, extra));
+
             //support
-            else if (type == Type.support)
-            {
-                Staffs.Add(new Support(name, email, empCode, extra));
-            }
+            else if (type == Type.support) Staffs.Add(new Support(name, email, empCode, extra));
         }
 
         public List<dynamic> GetAll()
@@ -32,12 +26,7 @@ namespace school
 
         public dynamic GetOne(int empCode)
         {
-            for (int i=0; i<Staffs.Count; i++)
-            {
-                if (Staffs[i].EmpCode == empCode) return Staffs[i];
-            }
-
-            throw new StaffNotFoundException();
+            return Staffs.Single(staff => staff.EmpCode == empCode);
         }
 
         public bool Update(int empCode, string name, string email, string extra)
@@ -61,15 +50,8 @@ namespace school
 
         public bool Delete(int empCode)
         {
-            for (int i = 0; i < Staffs.Count; i++)
-            {
-                if (Staffs[i].EmpCode == empCode)
-                {
-                    Staffs.RemoveAt(i);
-                    return true;
-                }
-            }
-            return false;
+            Staffs = Staffs.Where(staff => staff.EmpCode != empCode).ToList();
+            return true;
         }
     }
 }
