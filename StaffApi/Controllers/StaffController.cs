f64b89f1598de429d0f026e18be107d24c071413
vishnu <vishnuramesh19@gmail.com>
2020-11-19 15:07:23 +0530

    converted class objects to json parameters, changed extra to specific fields, check added for duplicate empCode

--- StaffApi/Controllers/StaffController.cs
+++ StaffApi/Controllers/StaffController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using school;
 using StaffApi.Models;
+using Newtonsoft.Json.Linq;
+using System;
 
 namespace StaffApi.Controllers
 {
@@ -22,9 +24,9 @@ namespace StaffApi.Controllers
         {
             List<dynamic> staffs = db.GetAll();
 
-            if (type == "teacher") return Ok(staffs.Where(staff => staff.Type == StaffType.teacher));
-            else if (type == "administrator") return Ok(staffs.Where(staff => staff.Type == StaffType.administrator));
-            else if (type == "support") return Ok(staffs.Where(staff => staff.Type == StaffType.support));
+            if (type == nameof(StaffType.teacher)) return Ok(staffs.Where(staff => staff.Type == StaffType.teacher));
+            else if (type == nameof(StaffType.administrator)) return Ok(staffs.Where(staff => staff.Type == StaffType.administrator));
+            else if (type == nameof(StaffType.support)) return Ok(staffs.Where(staff => staff.Type == StaffType.support));
             else if (type == null) return Ok(db.GetAll());
             else return NotFound();
         }
@@ -32,25 +34,65 @@ namespace StaffApi.Controllers
         [HttpGet("{empcode:int}")]
         public ActionResult Get(int empCode)
         {
-            dynamic staff = db.GetOne(empCode);
+            dynamic staff = db.GetOne(empCode); 
             if (staff == null) return NotFound();
             else return Ok(staff);
         }
 
         [HttpPost]
-        public ActionResult Post([FromBody] StaffAddObject staff)
+        public ActionResult Post([FromBody] JObject staff)
         {
-            db.AddStaff(staff.Type, staff.Name, staff.Email, staff.EmpCode, staff.Extra);
-            return Created("Staff Added", new Staff(staff.Name, staff.Email, staff.EmpCode, staff.Type));
+            dynamic st = db.GetOne((int)staff["empcode"]);
+
+            //if such staff exsists then its duplicate entry
+            if (st != null) return Conflict();
+            
+            dynamic returnObject;
+            string extra;
+
+            if ((int)staff["type"] == (int)StaffType.teacher)
+            {
+                extra = (string)staff["subject"];
+                returnObject = new Teacher((string)staff["name"], (string)staff["email"], (int)staff["empcode"], (string)staff["subject"]);
+            }
+            else if ((int)staff["type"] == (int)StaffType.administrator)
+            {
+                extra = (string)staff["role"];
+                returnObject = new Administrator((string)staff["name"], (string)staff["email"], (int)staff["empcode"], (string)staff["role"]);
+            }
+            else if ((int)staff["type"] == (int)StaffType.support)
+            {
+                extra = (string)staff["department"];
+                returnObject = new Support((string)staff["name"], (string)staff["email"], (int)staff["empcode"], (string)staff["department"]);
+            }
+            else return BadRequest();
+
+            //incorrect format
+            if (extra == null) return BadRequest();
+
+            db.AddStaff((StaffType)Enum.ToObject(typeof(StaffType), (int)staff["type"]), (string)staff["name"], (string)staff["email"], (int)staff["empcode"], extra);
+
+            return Created("Staff Added", returnObject);
         }   
 
         [HttpPut("{empcode:int}")]
-        public ActionResult Put(int empCode, [FromBody] StaffUpdateObject staff)
+        public ActionResult Put(int empCode, [FromBody] JObject staff)
         {
             dynamic st = db.GetOne(empCode);
             if (st == null) return NotFound();
 
-            return Ok(db.Update(empCode, staff.Name, staff.Email, staff.Extra));
+            string extra = null;
+
+            if (typeof(Teacher) == st.GetType()) extra = (string)staff["subject"];
+            else if (typeof(Support) == st.GetType()) extra = (string)staff["department"];
+            else if (typeof(Administrator) == st.GetType()) extra = (string)staff["role"];
+
+            //incorrect format
+            if (extra == null) return BadRequest();
+
+            db.Update(empCode, (string)staff["name"], (string)staff["email"], extra);
+
+            return Ok();
         }
 
         [HttpDelete("{empcode}")]
@@ -59,7 +101,9 @@ namespace StaffApi.Controllers
             dynamic st = db.GetOne(empCode);
             if (st == null) return NotFound();
 
-            return Ok(db.Delete(empCode));
+            db.Delete(empCode);
+
+            return Ok();
         }
     }
 }
